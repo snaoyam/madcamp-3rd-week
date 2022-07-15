5240779febee024defa2cd002c04943d88f3a844
jeong eun <koyy001@postech.ac.kr>
2022-07-15 19:18:25 +0900

    ãupload

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -1,22 +1,45 @@
-from flask import Flask, jsonify, render_template, request
-from flask_cors import CORS
+import os
+from io import BytesIO
+
+from flask import Flask, render_template, request, send_file
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-def create_app(config=None):
-  app = Flask(__name__)
-  app.config.update(dict(DEBUG=True))
-  app.config.update(config or {})
-  CORS(app)
-  
-  @app.route('/', defaults={'path': ''}, methods=['GET'])
-  def getTest(path):
-    return jsonify("asd", "Asd")
+class Upload(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(50))
+    data = db.Column(db.LargeBinary)
 
 
-  return app
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
-if __name__ == "__main__":
-  port = int(3000)
-  app = create_app()
-  app.run(port=port)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+
+        upload = Upload(filename=file.filename, data=file.read())
+        db.session.add(upload)
+        db.session.commit()
+
+        return f'Uploaded: {file.filename}'
+    return render_template('index.html')
+
+
+@app.route('/download/<upload_id>')
+def download(upload_id):
+    upload = Upload.query.filter_by(id=upload_id).first()
+    return send_file(BytesIO(upload.data), attachment_filename=upload.filename, as_attachment=True)
+
+if __name__ == '__main__':
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
+        app.run()
