9c458e687db53058b4068a8a295b55e6931e7e30
star <snaoyam@gmail.com>
2022-07-16 15:24:32 +0900

    multiple file upload

--- frontend-next/components/drag_n_drop.tsx
+++ frontend-next/components/drag_n_drop.tsx
@@ -1,61 +1,68 @@
-import React, { RefObject, useEffect } from 'react' 
+import React, { useEffect, useState, DragEvent, ChangeEvent } from 'react'
 import { Box } from '@mui/material'
-import { FiveK } from '@mui/icons-material'
+import { Document, Page } from 'react-pdf'
 
 const DragNDrop = () => {
-  const [dragActive, setDragActive] = React.useState<boolean>(false)
+  const [dragActive, setDragActive] = useState<boolean>(false)
   const fileInputRef = React.useRef<HTMLInputElement | null>(null)
-  const [dataTransfer, setDataTransfer] = React.useState<DataTransfer>()
-  useEffect(() => {
-    setDataTransfer(new DataTransfer())
-  }, [])
+  const [dataTransferList, setDataTransferList] = useState<File[]>([])
 
-  const handleDragEnter = function (e: React.DragEvent<HTMLInputElement>) {
+  const [itemsPerRow, setItemsPerRow] = useState<number>(3)
+
+  useEffect(() => {
+    const dataTransfer = new DataTransfer()
+    dataTransferList.forEach(file => {
+      dataTransfer.items.add(file)
+    })
+    if (fileInputRef.current) {
+      fileInputRef.current.files = dataTransfer.files
+    }
+  }, [dataTransferList])
+
+  const handleDragEnter = function (e: DragEvent<HTMLInputElement>) {
     e.preventDefault()
     e.stopPropagation()
     setDragActive(true)
   }
 
-  const handleDragLeave = function (e: React.DragEvent<HTMLInputElement>) {
+  const handleDragLeave = function (e: DragEvent<HTMLInputElement>) {
     e.preventDefault()
     e.stopPropagation()
     setDragActive(false)
   }
 
-  const handleDrop = function (e: React.DragEvent<HTMLInputElement>) {
-    console.log(2)
+  const handleDrop = function (e: DragEvent<HTMLInputElement>) {
     e.preventDefault()
     e.stopPropagation()
     setDragActive(false)
     const { files } = e.dataTransfer
+    
     if (files && files.length) {
       Array.from(files).forEach(file => {
-        let tempDataTransfer = dataTransfer
-        tempDataTransfer?.items.add(file)
-        setDataTransfer(tempDataTransfer)
-        
+        setDataTransferList((list) => [...list, file])
+        //dataTransfer?.items.add(file)
       })
-      if (fileInputRef.current && dataTransfer) {
-        fileInputRef.current.files = dataTransfer.files
-      }
     }
   }
 
-  const handleInputChange = function (e: React.ChangeEvent<HTMLInputElement>) {
-    console.log(e)
+  const handleInputChange = function (e: ChangeEvent<HTMLInputElement>) {
     e.preventDefault()
     const { files } = e.target;
     if (files && files.length) {
       for (let i = 0; i < (fileInputRef.current?.files?.length ?? 0); i++) {
-        let tempDataTransfer = dataTransfer
-        tempDataTransfer?.items.add(fileInputRef.current?.files?.item(i)!)
-        setDataTransfer(tempDataTransfer)
+        setDataTransferList((list) => [...list, fileInputRef.current?.files?.item(i)!])
       }
     }
-    if (fileInputRef.current && dataTransfer) {
-      fileInputRef.current.files = dataTransfer.files
-    }
   }
+  /*<Box
+              className={dragActive ? "drag-active" : ""}
+              sx={{
+                width: 402,
+                height: 80,
+                backgroundColor: 'green',
+              }}>
+  
+            </Box>*/
 
   return (
     <Box
@@ -81,24 +88,56 @@ const DragNDrop = () => {
           justifyContent: 'center',
         }}>
         <Box sx={{
+          //display: 'none',
+        }}>
+          <input ref={fileInputRef} onClick={(e) => { (e.target as HTMLInputElement).value = '' }} type="file" multiple={true} onChange={handleInputChange} />
+        </Box>
+        <Box sx={{
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
         }}>
-          <Box
-            className={dragActive ? "drag-active" : ""}
-            sx={{
-              width: 402,
-              height: 80,
-              backgroundColor: 'green',
-            }}>
 
-          </Box>
         </Box>
         <Box sx={{
-          display: 'none',
+          width: '100%',
+          height: '100%',
         }}>
-          <input ref={fileInputRef} type="file" multiple={true} onChange={handleInputChange} />
+          <Box sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'flex-start',
+            width: '100%',
+          }}>
+            {dataTransferList.map(v => {
+              return (
+                <Box sx={{
+                  width: `calc(100% / ${itemsPerRow})`,
+                  paddingTop: `calc(120% / ${itemsPerRow})`,
+                  position: 'relative',
+                  height: 0,
+                }}>
+                  <Box sx={{
+                    position: 'absolute',
+                    top: 0,
+                    width: '100%',
+                    height: '100%',
+                    padding: '10px',
+                  }}>
+                    <Box sx={{
+                      width: '100%',
+                      height: '100%',
+                      backgroundColor: 'green',
+                    }}>
+                      {/*<Document file="somefile.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+                        <Page pageNumber={pageNumber} />
+                      </Document>*/}
+                    </Box>
+                  </Box>
+                </Box>
+              )
+            })}
+          </Box>
         </Box>
       </Box>
     </Box>
--- frontend-next/pages/index.tsx
+++ frontend-next/pages/index.tsx
@@ -12,7 +12,6 @@ const Home: NextPage = () => {
     <Box
       sx={{
         width: 500,
-        height: 400,
         margin: '50px 40px',
         backgroundColor: 'rgb(255, 253, 219)',
       }}>
